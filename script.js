4417d878c9739807d6b1519cfff04c2f5bee0447
Jordan Stammer <s.jordanstammer@gmail.com>
2023-10-14 21:48:36 -0400

    Add resetGrid Function
    The resetGrid function was implemented to ensure the grid is being reset
    when adjusting the size. Previously, new boxes were being added on top
    of existing ones.

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 let container = document.querySelector('.container')
 
+
+
 function addBox(){
     let box = document.createElement('div');
     box.setAttribute('class', 'smallBox')
     container.appendChild(box);
 }
 
-for(let i = 0; i < 256; i ++){
-    console.log(i);
-    addBox();
+function resetGrid(){
+    let boxes = container.querySelectorAll('.smallBox');
+    boxes.forEach((box) =>{
+        container.removeChild(box);
+    })
 }
 
-let boxes = container.querySelectorAll('.smallBox')
-
+etchASketch = () => {let boxes = container.querySelectorAll('.smallBox')
 boxes.forEach((box)=> box.addEventListener('mouseenter', () =>{
     box.style.backgroundColor = 'blue';
-}))
+}))}
+
+createGrid = (gridSize = 256) => {for(let i = 0; i < (gridSize); i ++){
+    console.log(i);
+    addBox();
+}}
+
+
+createGrid();
+etchASketch();
+
+let button = document.querySelector('button');
+button.addEventListener('click', () => {
+    resetGrid();
+    gridSize = prompt(`Adjust Grid Size (1x1 - 100x100)`)
+    createGrid(gridSize);
+    etchASketch();
+});
