76e96cbbf245f74b6d8f3034712c00d34dadb42a
Jordan Stammer <s.jordanstammer@gmail.com>
2023-10-14 23:09:11 -0400

    Remove Test Files
    Removed test folder that was used to troubleshooting different solutions
    and not part of product.

--- test/test.js
+++
@@ -1,7 +0,0 @@
-let div = document.querySelector('div');
-console.log('div')
-
-
-let fiveHundred = 300
-div.addEventListener('click', () => div.style.cssText =`height: ${fiveHundred}px; width: ${fiveHundred}px;`)
-
